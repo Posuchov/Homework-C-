f70d12da3ca858445a4383d11d27d2ab3bc4667a
Posuchov <135158331+Posuchov@users.noreply.github.com>
2023-06-13 12:24:25 +0200

    Task 6-13.06.2023

---
+++ Task 6_13.06.2023/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 6:Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).//
+// 4 -> да
+//-3 -> нет
+// 7 -> нет
+
+Console.Clear();
+Console.Write("Input a = ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Input b = ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Input c = ");
+int c = int.Parse(Console.ReadLine());
+  if (a%2==0)
+{Console.WriteLine($"even number а = {a}");}
+  if (b%2==0)
+{Console.WriteLine($"even number b = {b}");}
+  if (c%2==0)
+{Console.WriteLine($"even number c = {c}");}
