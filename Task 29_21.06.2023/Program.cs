43bbe72562d6c7f3ecc4b7d44622d0be1ef5566d
Posuchov <posuchov@web.de>
2023-06-21 21:35:28 +0200

    Task 29_21.06.2023

---
+++ Task 29_21.06.2023/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+Console.Clear();
+int N = 8;
+int[]array = new int[N];
+
+FillArray(array);
+PrintArray(array);
+
+void FillArray(int[] arr)
+
+{
+    Random random = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] =random.Next(100);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
