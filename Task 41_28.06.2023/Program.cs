7c32f9be40d542226cc8b68a68545cb2e207e775
Posuchov <posuchov@web.de>
2023-06-28 18:03:50 +0200

    Task 41_28.06.2023

---
+++ Task 41_28.06.2023/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Input array length N = ");
+int a = int.Parse(Console.ReadLine()!);
+int count = 0;
+
+for(int i = 0; i < a; i++)
+{
+    Console.WriteLine($"input {i+1} number");
+    int b = int.Parse(Console.ReadLine()!);
+    if (b>0)count++;
+}
+Console.WriteLine($"{count} number > 0");
