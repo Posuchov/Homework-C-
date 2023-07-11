e8b8b62e033f4fecabbc64383c8baa631dc1a3eb
Posuchov <posuchov@web.de>
2023-07-11 19:20:19 +0200

    Task 66_11.07.2023

---
+++ Task 66_11.07.2023/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+using System;
+using static System.Console;
+Clear();
+WriteLine("Input M. ");
+int a = int.Parse(ReadLine()!);
+
+WriteLine("Input N. ");
+int b = int.Parse(ReadLine()!);
+
+WriteLine(PrintNumbers(a, b));
+
+int PrintNumbers(int a, int b)
+{
+    if (b == a || b == 1)
+    {
+        return 1;
+    }
+    int s = PrintNumbers(a, b - 1)+b;
+    return s;
+}
